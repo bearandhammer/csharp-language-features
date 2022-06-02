cac88c86e57cf58f7e3b978517ff184bc4072026
Lewis Grint <lgrint1@yahoo.co.uk>
2022-06-02 10:47:43 +0100

    Added CSharp.Language.Features.V7 project.
    Basic .NET Core 3.1 console application added to express snippets/features.
    
    Will add in console applications per language feature version (this will home 7.x features). Will utilise .NET6 when needed for future projects (when the features dictate that).

---
+++ CSharp.Language.Features/CSharp.Language.Features.V7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Language.Features.V7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
