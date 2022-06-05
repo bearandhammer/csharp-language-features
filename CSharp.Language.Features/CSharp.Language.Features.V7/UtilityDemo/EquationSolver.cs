d9b0b2c4ee125d183c35af108653e5136a82b0f3
Lewis Grint <lgrint1@yahoo.co.uk>
2022-06-05 13:53:56 +0100

    Added in a new version method to illustrate syntax changes further.

--- CSharp.Language.Features/CSharp.Language.Features.V7/UtilityDemo/EquationSolver.cs
+++ CSharp.Language.Features/CSharp.Language.Features.V7/UtilityDemo/EquationSolver.cs
@@ -15,31 +15,87 @@ namespace CSharp.Language.Features.V7.UtilityDemo
         /// <param name="b">The equation value 'b'.</param>
         /// <param name="c">The equation value 'c'.</param>
         /// <returns>A <see cref="Tuple"/> representing the resolution.</returns>
-        internal static Tuple<double, double> SolveQuadraticClassicLocalFunctionSyntax(double a, double b, double c)
+        internal Tuple<double, double> SolveQuadraticClassicLocalFunctionSyntax(double a, double b, double c)
         {
             // Classic way to express an inline local function (we must provide unique argument names in the signature)
             var CalculateDiscriminant = new Func<double, double, double, double>(
                 (aa, bb, cc) => bb * bb - 4 * aa * cc);
 
             double disc = CalculateDiscriminant(a, b, c);
-            double rootDisc = Math.Sqrt(disc);
 
-            return Tuple.Create<double, double>(
-                (-b - rootDisc) / (2 * a),
-                (-b + rootDisc) / (2 * a));
+            return GetQuadraticResolutionTuple(a, b, disc);
+        }
+
+        /// <summary>
+        /// Illustrates the new syntax of a local function (v1) to solve an equation (illustration only).
+        /// </summary>
+        /// <param name="a">The equation value 'a'.</param>
+        /// <param name="b">The equation value 'b'.</param>
+        /// <param name="c">The equation value 'c'.</param>
+        /// <returns>A <see cref="Tuple"/> representing the resolution.</returns>
+        internal Tuple<double, double> SolveQuadraticNewLocalFunctionSyntaxOne(double a, double b, double c)
+        {
+            // Define a local function, with unique argument names (signature) - can be expression-bodied of course
+            double CalculateDiscriminant(double aa, double bb, double cc)
+            {
+                return bb * bb - 4 * aa * cc;
+            }
+
+            double disc = CalculateDiscriminant(a, b, c);
+
+            return GetQuadraticResolutionTuple(a, b, disc);
+        }
+
+        /// <summary>
+        /// Illustrates the new syntax of a local function (v2) to solve an equation (illustration only).
+        /// </summary>
+        /// <param name="a">The equation value 'a'.</param>
+        /// <param name="b">The equation value 'b'.</param>
+        /// <param name="c">The equation value 'c'.</param>
+        /// <returns>A <see cref="Tuple"/> representing the resolution.</returns>
+        internal Tuple<double, double> SolveQuadraticNewLocalFunctionSyntaxTwo(double a, double b, double c)
+        {
+            // Define a local function with no arguments using the chained in arguments to the method directly (refactored to expression-bodied this this time)
+            double CalculateDiscriminant() => b * b - 4 * a * c;
+
+            // Then, when using the function, we don't provide parameters (as the local function has used the raw arguments)
+            double disc = CalculateDiscriminant();
+
+            return GetQuadraticResolutionTuple(a, b, disc);
         }
 
         ///// <summary>
-        ///// Illustrates the new syntax of a local function (v1) to solve an equation (illustration only).
+        ///// Illustrates the new syntax of a local function (v2) to solve an equation (illustration only).
         ///// </summary>
         ///// <param name="a">The equation value 'a'.</param>
         ///// <param name="b">The equation value 'b'.</param>
         ///// <param name="c">The equation value 'c'.</param>
         ///// <returns>A <see cref="Tuple"/> representing the resolution.</returns>
-        //internal static Tuple<double, double> SolveQuadraticNewLocalFunctionSyntaxOne(double a, double b, double c)
+        //internal Tuple<double, double> SolveQuadraticNewLocalFunctionSyntaxThree(double a, double b, double c)
         //{
-        //    // Define a local function, with unique argument names (signature)
-        //    double CalculateDiscriminant
+        //    // Then, when using the function, we don't provide parameters (as the local function has used the raw arguments)
+        //    double disc = CalculateDiscriminant();
+
+        //    return GetQuadraticResolutionTuple(a, b, disc);
+
+
+        //    double CalculateDiscriminant() => b * b - 4 * a * c;
         //}
+
+        /// <summary>
+        /// Gets a <see cref="Tuple"/> representing the result for all of the demo methods in this type.
+        /// </summary>
+        /// <param name="a">The equation value 'a'.</param>
+        /// <param name="b">The equation value 'b'.</param>
+        /// <param name="disc">The discrimnator value.</param>
+        /// <returns>A <see cref="Tuple"/> illustrating the result.</returns>
+        private static Tuple<double, double> GetQuadraticResolutionTuple(double a, double b, double disc)
+        {
+            double rootDisc = Math.Sqrt(disc);
+
+            return Tuple.Create(
+                (-b - rootDisc) / (2 * a),
+                (-b + rootDisc) / (2 * a));
+        }
     }
 }
