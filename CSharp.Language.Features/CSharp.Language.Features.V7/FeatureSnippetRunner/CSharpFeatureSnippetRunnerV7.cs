ab11c8ce3e27fca7c649a86d076e4886b542bfe6
Lewis Grint <lgrint1@yahoo.co.uk>
2022-06-18 15:37:06 +0100

    Add basic demo for literal improvements in C# 7.

--- CSharp.Language.Features/CSharp.Language.Features.V7/FeatureSnippetRunner/CSharpFeatureSnippetRunnerV7.cs
+++ CSharp.Language.Features/CSharp.Language.Features.V7/FeatureSnippetRunner/CSharpFeatureSnippetRunnerV7.cs
@@ -302,6 +302,35 @@ namespace CSharp.Language.Features.V7.FeatureSnippetRunner
         }
 
         /// <summary>
+        /// Executes demo code showing literal improvements in C# V7.
+        /// </summary>
+        internal void ExecuteLiteralImprovementsFeatureSnippet()
+        {
+            // Small improvements to literals for numeric types, as follows (makes reading these less cumbersome) - useful for delimiting
+            int oneMillionLiteral = 1_000_000;
+
+            // The position of the underscores can be positioned where you like and you can have as many as you like (should you want to)
+            int numberOneLiteral = 129_9_21,
+                numberTwoLiteral = 129__321_____999;
+
+            // Underscores ignored by the compiler
+
+            // Trailing underscores are a 'nope' of course
+            //int trailingUnderscores = 150_000___;
+
+            // Works for hex/binary
+            long hexLiteral = 0xFF_FF_FF_FF;
+            int binaryLiteral = 0b_0010_1010;
+
+            // Inspect the values of the defined literals
+            Console.WriteLine($"oneMillionLiteral = {oneMillionLiteral}");
+            Console.WriteLine($"numberOneLiteral = {numberOneLiteral}");
+            Console.WriteLine($"numberTwoLiteral = {numberTwoLiteral}");
+            Console.WriteLine($"hexLiteral = {hexLiteral}");
+            Console.WriteLine($"binaryLiteral = {binaryLiteral}");
+        }
+
+        /// <summary>
         /// Demo utility method responsible for displaying a shape.
         /// </summary>
         /// <param name="shapeInScope">The current <see cref="Shape"/> to operate against.</param>
