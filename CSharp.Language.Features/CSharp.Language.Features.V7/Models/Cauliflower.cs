b1f42a0e6a52a25ad4505d188006e4708042a615
Lewis Grint <lgrint1@yahoo.co.uk>
2022-07-03 13:08:41 +0100

    Added two sample classes to illustrate pattern matching changes.

---
+++ CSharp.Language.Features/CSharp.Language.Features.V7/Models/Cauliflower.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.Language.Features.V7.Models
+{
+    /// <summary>
+    /// Class representing a cauliflower (for sample purposes).
+    /// </summary>
+    /// <seealso cref="Vegetable" />
+    internal sealed class Cauliflower : Vegetable
+    {
+    }
+}
---
+++ CSharp.Language.Features/CSharp.Language.Features.V7/Models/Vegetable.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp.Language.Features.V7.Models
+{
+    /// <summary>
+    /// Class representing a vegetable (for sample purposes).
+    /// </summary>
+    internal class Vegetable
+    {
+    }
+}
