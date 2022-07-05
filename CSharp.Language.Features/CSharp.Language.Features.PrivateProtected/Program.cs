5f2c4366fa910f1881822aa262d0c8d0759d0c38
Lewis Grint <lgrint1@yahoo.co.uk>
2022-07-05 12:15:43 +0100

    Private protected sample (project rigging).

---
+++ CSharp.Language.Features/CSharp.Language.Features.PrivateProtected/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp.Language.Features.PrivateProtected
+{
+    /// <summary>
+    /// <see cref="Program"/> class for demonstrate facets of the private protected access modifier.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        private static void Main()
+        {
+        }
+    }
+}
