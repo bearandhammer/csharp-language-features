58a44b5b41dbb7cdbb322c4d7f425310f3ac7af4
Lewis Grint <lgrint1@yahoo.co.uk>
2022-07-18 18:49:07 +0100

    Include a new benchmarking project.
    For .NET 7 features.

---
+++ CSharp.Language.Features/CSharp.Language.Features.V7.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Language.Features.V7.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
